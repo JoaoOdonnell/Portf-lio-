814ee0651447df299aff831ccc006f6751989fb7
João O'Donnell <talaiajoao211@gmail.com>
2022-10-07 19:42:54 -0300

    primeiro script adicionado

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+//GitHub
+function mouseOverG() {
+  document.getElementById("github").innerHTML = ">GitHub";
+}
+
+function mouseOutG() {
+  document.getElementById("github").innerHTML = "";
+}
+
+//Linkedin
+function mouseOverL() {
+  document.getElementById("linkedin").innerHTML = ">Linkedin";
+}
+
+function mouseOutL() {
+  document.getElementById("linkedin").innerHTML = "";
+}
+
+//Instagram
+function mouseOverI() {
+  document.getElementById("instagram").innerHTML = ">Instagram";
+}
+
+function mouseOutI() {
+  document.getElementById("instagram").innerHTML = "";
+}
+
+//Email
+function mouseOverE() {
+  document.getElementById("email").innerHTML = ">E-mail";
+}
+
+function mouseOutE() {
+  document.getElementById("email").innerHTML = "";
+}
+
+function mostrarProjetos() {
+  document.getElementById("linkGitHub").innerHTML = "https://github.com/JoaoOdonnell";
+}
