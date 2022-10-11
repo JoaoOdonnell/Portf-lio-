2938a1903286066cd15e801cc7a650cdacfbe702
João O'Donnell <talaiajoao211@gmail.com>
2022-10-10 23:43:51 -0300

    Script atualizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,18 @@
-//GitHub
-function mouseOverG() {
-  document.getElementById("github").innerHTML = ">GitHub";
-}
-
-function mouseOutG() {
-  document.getElementById("github").innerHTML = "";
-}
-
-//Linkedin
-function mouseOverL() {
-  document.getElementById("linkedin").innerHTML = ">Linkedin";
-}
-
-function mouseOutL() {
-  document.getElementById("linkedin").innerHTML = "";
-}
-
-//Instagram
-function mouseOverI() {
-  document.getElementById("instagram").innerHTML = ">Instagram";
-}
-
-function mouseOutI() {
-  document.getElementById("instagram").innerHTML = "";
-}
-
-//Email
-function mouseOverE() {
-  document.getElementById("email").innerHTML = ">E-mail";
-}
-
-function mouseOutE() {
-  document.getElementById("email").innerHTML = "";
+function mouseEvent(isover, elementId, overtext) {
+  if (isover) {
+    document.getElementById(elementId).innerHTML = `>${overtext}`;
+  } else {
+    document.getElementById(elementId).innerHTML = "";
+  }
 }
 
 function mostrarProjetos() {
   document.getElementById("linkGitHub").innerHTML = "https://github.com/JoaoOdonnell";
 }
+
+const triangulo = document.querySelector(".triangulo-para-baixo");
+const linkGit = document.querySelector("#linkGitHub");
+
+triangulo.addEventListener("click", () => {
+  linkGit.classList.toggle("hidden");
+});
